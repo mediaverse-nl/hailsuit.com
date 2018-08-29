d14c2dee15dd94867b95533b57d950be02088ddf
master <deveron-reniers@hotmail.com>
2018-08-29 09:48:31 +0200

    admin panel detail, brand and property layout functional

--- app/Http/Controllers/Admin/TextController.php
+++ app/Http/Controllers/Admin/TextController.php
@@ -14,7 +14,7 @@ class TextController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.text.index');
     }
 
     /**
--- resources/views/admin/seo/index.blade.php
+++ resources/views/admin/seo/index.blade.php
@@ -6,7 +6,7 @@
     <div class="card mb-3">
         <div class="card-header">
             <i class="fas fa-table"></i>
-            Product Table
+            SEO Table
         </div>
         <div class="card-body">
             <div class="table-responsive">
